7773f2e084f796a4cf52cc43e7c2bf036cb51140
Vitor Borges Jr <Vitor>
2021-02-07 13:13:27 -0300

    Update 04-Preprocessing-and-pipelines.py

--- Supervised-Learning-with-scikit-learn/04-Preprocessing-and-pipelines.py
+++ Supervised-Learning-with-scikit-learn/04-Preprocessing-and-pipelines.py
@@ -12,6 +12,7 @@
 import matplotlib.pyplot as plt
 
 # Import pandas
+import numpy as np
 import pandas as pd
 
 # Read 'gapminder.csv' into a DataFrame: df
@@ -38,19 +39,20 @@ print(df_region.columns)
 
 # Regression with categorical features
 # Import necessary modules
-____
-____
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
 
 # Instantiate a ridge regressor: ridge
-ridge = ____
+ridge = Ridge(alpha = 0.5, normalize = True)
 
 # Perform 5-fold cross-validation: ridge_cv
-ridge_cv = ____
+ridge_cv = cross_val_score(ridge, X, y, cv = 5)
 
 # Print the cross-validated scores
-print(____)
+print(ridge_cv)
 
-
+# Handling missing data ------------------------------------------------
+# Dropping missing data
 # convert '?' to NaN
 df[df == '?'] = np.nan
 
@@ -67,9 +69,166 @@ df = df.dropna()
 print("Shape of DataFrame After Dropping All Rows with Missing Values: {}".format(df.shape))
 
 # Steps to building a model:
+# 1 - Explore data for missing values
+# 2 - Imput appropriete data
+# ...
 # 1 - create training and test sets
 # 2 - fit a classifier or regressor
-# 3 - tuning its parameters
+# 3 - tuning its parameters and hyperperameters
 # 4 - evaluating its performance on new data
 
 # Imputing missing data in a ML pipeline I
+# Import the Imputer module
+from sklearn.preprocessing import Imputer
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+
+# Setup the Imputation transformer: imp
+imp = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0)
+
+# Instantiate the SVC classifier: clf
+clf = SVC()
+
+# Setup the pipeline with the required steps: steps
+steps = [('imputation', imp),
+        ('SVM', clf)]
+
+# Imputing missing data in a ML Pipeline II
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+# Fit the pipeline to the train set
+pipeline.fit(X_train, y_train)
+
+# Predict the labels of the test set
+y_pred = pipeline.predict(X_test)
+
+# Compute metrics
+print(classification_report(y_test, y_pred))
+
+# Centering and scaling your data
+df = pd.read_csv('datasets/white-wine.csv')
+df.info()
+y = df['quality'].values.copy()
+y[y < 5] = True
+y[y >= 5] = False
+y.shape
+X = df.drop('quality', axis = 1).values
+X.shape
+df.describe()
+
+# Import scale
+from sklearn.preprocessing import scale
+
+# Scale the features: X_scaled
+X_scaled = scale(X)
+
+# Print the mean and standard deviation of the unscaled features
+print("Mean of Unscaled Features: {}".format(np.mean(X))) 
+print("Standard Deviation of Unscaled Features: {}".format(np.std(X)))
+
+# Print the mean and standard deviation of the scaled features
+print("Mean of Scaled Features: {}".format(np.mean(X_scaled))) 
+print("Standard Deviation of Scaled Features: {}".format(np.std(X_scaled)))
+
+# Centering and scaling in a pipeline
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# Import the necessary modules
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# Setup the pipeline steps: steps
+steps = [('scaler', StandardScaler()),
+        ('knn', KNeighborsClassifier())]
+        
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+# Fit the pipeline to the training set: knn_scaled
+knn_scaled = pipeline.fit(X_train, y_train)
+
+# Instantiate and fit a k-NN classifier to the unscaled data
+knn_unscaled = KNeighborsClassifier().fit(X_train, y_train)
+
+# Compute and print metrics
+print('Accuracy with Scaling: {}'.format(knn_scaled.score(X_test, y_test)))
+print('Accuracy without Scaling: {}'.format(knn_unscaled.score(X_test, y_test)))
+
+# Bringing it all together I: Pipeline for classification
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import GridSearchCV
+
+# Setup the pipeline
+steps = [('scaler', StandardScaler()),
+         ('SVM', SVC())]
+
+pipeline = Pipeline(steps)
+
+# Specify the hyperparameter space
+parameters = {'SVM__C':[1, 10, 100],
+              'SVM__gamma':[0.1, 0.01]}
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 21)
+
+# Instantiate the GridSearchCV object: cv
+cv = GridSearchCV(pipeline, parameters, cv = 3)
+
+# Fit to the training set
+cv.fit(X_train, y_train)
+
+# Predict the labels of the test set: y_pred
+y_pred = cv.predict(X_test)
+
+# Compute and print metrics
+print("Accuracy: {}".format(cv.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+print("Tuned Model Parameters: {}".format(cv.best_params_))
+
+# Bringing it all together II: Pipeline for regression
+
+
+# Read the CSV file into a DataFrame: df
+df = pd.read_csv('datasets/gm_2008_region.csv', sep = ',')
+
+# Create arrays for features and target variable
+y = df['life'].values
+X = df.drop('life', axis = 1).values
+
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)),
+         ('scaler', StandardScaler()),
+         ('elasticnet', ElasticNet())]
+
+# Create the pipeline: pipeline 
+pipeline = Pipeline(steps)
+
+# Specify the hyperparameter space
+parameters = {'elasticnet__l1_ratio': np.linspace(0,1,30)}
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+
+# Create the GridSearchCV object: gm_cv
+gm_cv = GridSearchCV(pipeline, parameters, cv = 3)
+
+# Fit to the training set
+gm_cv.fit(X_train, y_train)
+
+# Compute and print the metrics
+r2 = gm_cv.score(X_test, y_test)
+print("Tuned ElasticNet Alpha: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
